6f429da2ba4f3c9d2b3f7a19c1c0ae07e84460bd
Grant Hyun Park <gpark18@amherst.edu>
2015-07-11 21:16:26 -0700

    Done

--- magic.js
+++ magic.js
@@ -14,7 +14,77 @@ jQuery(document).ready(function($) {
 		  var location = parsed_json['location']['city'];
 		  var temp_f = parsed_json['current_observation']['temp_f'];
 		  document.getElementById("weather").innerHTML = "Current temperature in " + location + " is " + temp_f + "&#176" + "F" + " and it's " + weather.toLowerCase() + ".";
-		  $(window).trigger('resize');
+
+		  var mdiv = document.getElementById("mpics");
+		  var fdiv = document.getElementById("fpics");
+		  var mTag = document.createElement('img');
+		  var fTag = document.createElement('img');
+
+		  mTag.width = 300;
+		  mTag.height = 300;
+		  fTag.width = 300;
+		  fTag.height = 300;
+
+		  mTag.style.borderRadius = "100px"
+		  fTag.style.borderRadius = "100px"
+
+		  mTag.style.webkitBoxShadow = "0px 0px 10px 4px rgba(119, 119, 119, 0.75)"
+		  fTag.style.webkitBoxShadow = "0px 0px 10px 4px rgba(119, 119, 119, 0.75)"
+
+
+		  if (temp_f > 80) {
+		  	mTag.src = "temp/mabove80.jpg";
+		  	fTag.src = "temp/fabove80.jpg";
+		  	document.getElementById("mpics").appendChild(mTag);
+		  	document.getElementById("fpics").appendChild(fTag);
+		  }
+		  else if (temp_f < 80 && temp_f > 60) {
+		  	fTag.src = "temp/f60thru80.jpg";
+		  	mTag.src = "temp/m50thru80.jpg";
+		  	document.getElementById("fpics").appendChild(fTag);
+		  	document.getElementById("mpics").appendChild(mTag);
+		  }
+		  else if (temp_f < 60) {
+		  	fTag.src = "temp/fbelow60.jpg";
+		  	mTag.src = "temp/m50thru80.jpg";
+		  	document.getElementById("fpics").appendChild(fTag);
+		  	document.getElementById("mpics").appendChild(mTag);
+		  }
+		  else if (temp_f < 50) {
+		  	fTag.src = "temp/fbelow60.jpg";
+		  	mTag.src = "temp/mbelow50.jpg";
+		  	document.getElementById("fpics").appendChild(fTag);
+		  	document.getElementById("mpics").appendChild(mTag);
+		  }
+
 		}
 		  });
+
+		
+			$('img').each(function() {
+		        var maxWidth = 100; // Max width for the image
+		        var maxHeight = 100;    // Max height for the image
+		        var ratio = 0;  // Used for aspect ratio
+		        var width = $(this).width();    // Current image width
+		        var height = $(this).height();  // Current image height
+
+		        // Check if the current width is larger than the max
+		        if(width > maxWidth){
+		            ratio = maxWidth / width;   // get ratio for scaling image
+		            $(this).css("width", maxWidth); // Set new width
+		            $(this).css("height", height * ratio);  // Scale height based on ratio
+		            height = height * ratio;    // Reset height to match scaled image
+		            width = width * ratio;    // Reset width to match scaled image
+		        }
+
+		        // Check if current height is larger than max
+		        if(height > maxHeight){
+		            ratio = maxHeight / height; // get ratio for scaling image
+		            $(this).css("height", maxHeight);   // Set new height
+		            $(this).css("width", width * ratio);    // Scale width based on ratio
+		            width = width * ratio;    // Reset width to match scaled image
+		            height = height * ratio;    // Reset height to match scaled image
+		        }
+		    });
 		});
+
