734ed20a6c6cffbbde79497990c88820cc7e5d78
sungjp <gpark18@amherst.edu>
2016-06-25 15:43:57 -0400

    added browserify and refactored gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,35 +6,47 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     jade = require('gulp-jade'),
     jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+    stylish = require('jshint-stylish'),
+    browserify = require('browserify');
 
-gulp.task('styles',function() {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('css'));
-});
+var tasks = {
+    styles: function(){
+      gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('css'));
+    },
+    minifyCSS: function(){
+      return gulp.src('css/**/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./prod/css/'));
+    },
+    templates: function(){
+      gulp.src('jade/**/*.jade')
+        .pipe(jade({}))
+        .pipe(gulp.dest('.'));
+    },
+    lintjs: function(){
+      return gulp.src(['Gulpfile.js','js/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .on('error', function(){
+          console.log("afplay " + file);
+          exec("afplay " + file);
+        });
+    },
+    browserify: function(){
+      //Mess with for later
+      var b = browserify();
+      b.add('prod/js/app.js');
+      b.bundle().pipe(process.stdout);
+    }
+};
 
-gulp.task('minify-css', function() {
-  return gulp.src('css/**/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./prod/css/'));
-});
-
-gulp.task('templates',function(){
-  gulp.src('jade/**/*.jade')
-    .pipe(jade({}))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('lintjs',function(){
-  return gulp.src(['Gulpfile.js','js/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .on('error', function(){
-      console.log("afplay " + file);
-      exec("afplay " + file);
-    });
-});
+// Tasks
+gulp.task('styles',tasks.styles);
+gulp.task('minifyCSS',tasks.minifyCSS);
+gulp.task('templates',tasks.templates);
+gulp.task('lintjs',tasks.lintjs);
 
 // Work from sass, Production from prod/css
 gulp.task('watch',function() {
